60ac15edfce71e100b8fceedd0b5d633ec49521e
crissiiu <crissiiu.dev@gmail.com>
2026-06-18 16:59:12 +0700

    Done build Shared Library for Common Service
    - Response
    - Logs
    - Interface
    - Middleware
    - DependenceInjection

---
+++ EdTechSolution/EdTech.SharedLibrary/Interface/IGenericInterface.cs
@@ -0,0 +1,20 @@
+﻿using EdTech.SharedLibrary.Responses;
+using System.Linq.Expressions;
+
+namespace EdTech.SharedLibrary.Interface
+{
+    /// <summary>
+    /// Interface mẫu generic repository dùng chung cho các tác vụ CRUD cơ bản của mọi serive
+    /// </summary>
+    public interface IGenericInterface<T> where T: class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync();
+        Task<Response> CreateAsync();
+        Task<Response> UpdateAsync();
+        Task<Response> DeleteAsync();
+
+        //Hàm tìm kiếm nâng cao bằng biểu thức Lamda (Expression)
+        Task<T?> GetByExpressionAsync(Expression<Func<T, bool>> expression);
+    }
+}
---
+++ EdTechSolution/EdTech.SharedLibrary/Logs/LogException.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Serilog;
+
+namespace EdTech.SharedLibrary.Logs
+{
+    public static class LogException
+    {
+        /// <summary>
+        /// Hàm tiện ích hỗ trợ ghi log cấu trúc thông qua Serilog xuống Console và File
+        /// </summary>
+        public static void LogExceptions(Exception ex, HttpContext context)
+        {
+            Log.Error(ex, 
+                $"============== [EDTECH ERROR LOG] ==============\n" +
+                $"Thời gian: {DateTime.UtcNow}\n" +
+                $"Đường dẫn API: {context.Request.Path}\n" +
+                $"Phương thức: {context.Request.Method}\n" +
+                $"Thông điệp lỗi: {ex.Message}\n" +
+                "================================================"
+            );
+        }
+    }
+}
